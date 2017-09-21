cbaa58a57919fe9d72034d4dd09de2fd31dde7fa
YG <yg@YGdeiMac.local>
2017-09-21 10:15:57 +0800

    delete ZXing ZBar

--- Podfile
+++ Podfile
@@ -2,7 +2,6 @@ platform:ios, '8.0'
 
 target 'YouguRead' do
   pod 'ReactiveCocoa', '2.5'
-  #pod 'AFNetworking', '3.1.0'
   pod 'MJRefresh', '3.1.12'
   pod 'MJExtension', '3.0.13'
   pod 'Masonry', '1.0.2'
@@ -11,12 +10,11 @@ target 'YouguRead' do
   pod 'PYSearch', '0.8.3'
   pod 'ZYCornerRadius', '1.0.2'
   pod 'LBXScan/LBXNative','~> 2.2'
-  pod 'LBXScan/LBXZXing','~> 2.2'
-  pod 'LBXScan/LBXZBar','~> 2.2'
   pod 'LBXScan/UI','~> 2.2'
   pod 'DZNEmptyDataSet', '1.8.1'
   pod 'FMDB', '2.7.2'
   pod 'MBProgressHUD', '1.0.0'
   pod 'YTKNetwork', '2.0.4'
+  #pod 'AFNetworking', '3.1.0'
 end
 
--- Pods/Target Support Files/Pods-YouguRead/Pods-YouguRead-resources.sh
+++ Pods/Target Support Files/Pods-YouguRead/Pods-YouguRead-resources.sh
@@ -87,19 +87,11 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/LBXScan/LBXScan/UI/CodeScan.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
   install_resource "${PODS_ROOT}/PYSearch/PYSearch/PYSearch.bundle"
-  install_resource "${PODS_ROOT}/ZBarSDK/iphone/res/zbar-back.png"
-  install_resource "${PODS_ROOT}/ZBarSDK/iphone/res/zbar-help.html"
-  install_resource "${PODS_ROOT}/ZBarSDK/iphone/res/zbar-helpicons.png"
-  install_resource "${PODS_ROOT}/ZBarSDK/iphone/res/zbar-samples.png"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/LBXScan/LBXScan/UI/CodeScan.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
   install_resource "${PODS_ROOT}/PYSearch/PYSearch/PYSearch.bundle"
-  install_resource "${PODS_ROOT}/ZBarSDK/iphone/res/zbar-back.png"
-  install_resource "${PODS_ROOT}/ZBarSDK/iphone/res/zbar-help.html"
-  install_resource "${PODS_ROOT}/ZBarSDK/iphone/res/zbar-helpicons.png"
-  install_resource "${PODS_ROOT}/ZBarSDK/iphone/res/zbar-samples.png"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
